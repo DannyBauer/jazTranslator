4abc973d69282e2b7c1eb6a2b4deae655e788e90
Daniel Bauer <d.bauer@plastictechnologies.com>
2018-02-21 14:23:43 -0500

    rough but functional show translation

--- jazTranslator/Program.cs
+++ jazTranslator/Program.cs
@@ -1,25 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace jazTranslator
 {
     class Program
     {
+        static List<string> outputLines = new List<string>();
         static void Main(string[] args)
         {
-            ReadFile();
-        }
-
-        static void ReadFile()
-        {
             string line;
+            string instruction;
             StreamReader file = new StreamReader(@"E:\Project Files\jazTranslator\jazTranslator\jazTranslator\jaz specs and examples (1)\demo.jaz");
             while ((line = file.ReadLine()) != null)
             {
                 Console.WriteLine(line);
+                List<string> instructionArrary = line.Split().ToList();
+                instruction = instructionArrary[0];
+                switch (instruction)
+                {
+                    case "show":
+                        show(instructionArrary);
+                        break;
+                    default:
+                        break;
+
+                }
+
             }
-            file.Close();
+            if (File.Exists(@"E:\Project Files\jazTranslator\jazTranslator\jazTranslator\output.cpp"))
+            {
+                File.Delete(@"E:\Project Files\jazTranslator\jazTranslator\jazTranslator\output.cpp");
+            }
+
+            // Create a file to write to.
+            using (StreamWriter sw = File.CreateText(@"E:\Project Files\jazTranslator\jazTranslator\jazTranslator\output.cpp"))
+            {
+                for (int i = 0; i < outputLines.Count; i++)
+                {
+                    sw.WriteLine(outputLines[i]);
+                }
+                sw.Close();
+            }
+
             Console.ReadLine();
+        }
+        static void show(List<string> instruction)
+        {
+            string newInstruction = "";
+            int wordCount = instruction.Count;
+            newInstruction = newInstruction + "cout << \"";
+            for (int i = 1; i < wordCount; i++)
+            {
+                newInstruction = newInstruction + " " + instruction[i];
+            }
+            newInstruction = newInstruction + "\" << endl;";
+            outputLines.Add(newInstruction);
         }
     }
 }
---
+++ jazTranslator/output.cpp
@@ -0,0 +1,12 @@
+cout << " " << endl;
+cout << " This code illustrates parameter passing strategy." << endl;
+cout << " " << endl;
+cout << " ---------------------------------" << endl;
+cout << " after function work:" << endl;
+cout << " value of x is:" << endl;
+cout << " value of f is:" << endl;
+cout << " " << endl;
+cout << " before function work:" << endl;
+cout << " value of x is:" << endl;
+cout << " value of f is:" << endl;
+cout << " --------------------------------" << endl;
