613688f61abc8551d575b19b1dc2e22917fa3939
dbauer0706 <dbauer07061@gmail.com>
2018-02-26 13:56:42 -0500

    added variables to top of file and plus

--- jazTranslator/Program.cs
+++ jazTranslator/Program.cs
@@ -8,7 +8,10 @@ namespace jazTranslator
     class Program
     {
         static List<string> outputLines = new List<string>();
+        static List<string> variables = new List<string>();
+        static int variableCount = 0;
         static int iterator;
+        private static Random random = new Random();
         static void Main(string[] args)
         {
             initialzeProgram();
@@ -51,7 +54,18 @@ namespace jazTranslator
                     case "goto":
                         go_to(instructionList);
                         break;
-
+                    case "gofalse":
+                        gofalse(instructionList);
+                        break;
+                    case "gotrue":
+                        gotrue(instructionList);
+                        break;
+                    case "halt":
+                        halt();
+                        break;
+                    case "+":
+                        plus(instructionList);
+                        break;
                     default:
                         break;
 
@@ -67,7 +81,7 @@ namespace jazTranslator
             string newInstruction = "";
             int wordCount = instruction.Count;
             newInstruction = newInstruction + "cout << \"";
-            for (int i = iterator; i < wordCount; i++)
+            for (int i = iterator + 1; i < wordCount; i++)
             {
                 newInstruction = newInstruction + " " + instruction[i];
             }
@@ -87,8 +101,6 @@ namespace jazTranslator
         static void lvalue(List<string> instruction)
         {
             string variable = instruction[iterator + 1];
-            if (!outputLines.Contains("int " + variable + ";"))
-                outputLines.Add("int " + variable + ";");
             outputLines.Add("pointer_stack.push(&" + variable + ");");
         }
         static void equals()
@@ -114,6 +126,38 @@ namespace jazTranslator
         {
             outputLines.Add("goto label_" + instruction[iterator + 1] +";");
         }
+        static void gofalse(List<string> instruction)
+        {
+            outputLines.Add("if (int_stack.top() == 0) {");
+            outputLines.Add("goto label_" + instruction[iterator + 1] + "; }");
+            outputLines.Add("int_stack.pop();");
+        }
+        static void gotrue(List<string> instruction)
+        {
+            outputLines.Add("if (int_stack.top() != 0) {");
+            outputLines.Add("goto label_" + instruction[iterator + 1] + "; }");
+            outputLines.Add("int_stack.pop();");
+        }
+        static void halt()
+        {
+            outputLines.Add("return 0;");
+        }
+        static void plus(List<string> instruction)
+        {
+            string variable1 = variables[variableCount];
+            variableCount++;
+  
+            outputLines.Add( variable1 + " = int_stack.top();");
+            outputLines.Add("int_stack.pop();");
+
+            string variable2 = variables[variableCount];
+            variableCount++;
+
+            outputLines.Add( variable2 + " = int_stack.top();");
+            outputLines.Add("int_stack.pop();");
+            outputLines.Add("int_stack.push(" + variable1 + " + " + variable2 + ");");
+
+        }
         static void initialzeProgram()
         {
             if (File.Exists("output.cpp"))
@@ -121,7 +165,24 @@ namespace jazTranslator
                 File.Delete("output.cpp");
             }
 
-            using (StreamWriter sw = File.CreateText("output.cpp"))
+            string line;
+            string instruction;
+            StreamReader file = new StreamReader(@"jaz specs and examples (1)\demo.jaz");
+            while ((line = file.ReadLine()) != null)
+            {
+                List<string> instructionList = line.Split().ToList();
+                iterator = 0;
+                while (instructionList[iterator] == "" && instructionList.Count > 1)
+                    iterator++;
+                instruction = instructionList[iterator];
+
+                if (instruction == "lvalue" && !variables.Contains(instructionList[iterator + 1]))
+                {
+                    variables.Add(instructionList[iterator + 1]);
+                }
+            }
+
+                using (StreamWriter sw = File.CreateText("output.cpp"))
             {
                 sw.WriteLine("# include \"stdafx.h\"");
                 sw.WriteLine("# include <iostream>");
@@ -129,6 +190,11 @@ namespace jazTranslator
                 sw.WriteLine("# include <stack>");
                 sw.WriteLine("using namespace std;");
                 sw.WriteLine("int main() {");
+
+                for (int i  = 0; i < variables.Count; i++)
+                {
+                    sw.WriteLine("int " + variables[i] + ";");
+                }
                 sw.WriteLine("stack<int> int_stack;");
                 sw.WriteLine("stack<int*> pointer_stack;");
             }
@@ -144,6 +210,13 @@ namespace jazTranslator
                 sw.WriteLine("}");
                 sw.Close();
             }
+        }
+       
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 }
