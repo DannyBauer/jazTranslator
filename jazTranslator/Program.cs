4c4562d37631165424eb54a699cc7c1b37327546
dbauer0706 <dbauer07061@gmail.com>
2018-02-28 10:28:15 -0500

    stuff

--- jazTranslator/Program.cs
+++ jazTranslator/Program.cs
@@ -7,20 +7,23 @@ namespace jazTranslator
 {
     class Program
     {
+        static List<string> inputLines = new List<string>();
         static List<string> outputLines = new List<string>();
+        static List<string> functionLines = new List<string>();
         static List<string> variables = new List<string>();
+        static List<string> functions = new List<string>();
         static int variableCount = 0;
         static int iterator;
+        static int outputListTracker = 0; //0 for output lines and 1 for function lines
         private static Random random = new Random();
         static void Main(string[] args)
         {
-            initialzeProgram();
-            string line;
+            readJazLines();
+            initialzeProgram();       
             string instruction;
-            StreamReader file = new StreamReader(@"jaz specs and examples (1)\demo.jaz");
-            while ((line = file.ReadLine()) != null)
+            for (int i = 0; i < inputLines.Count(); i++)
             {
-                List<string> instructionList = line.Split().ToList();
+                List<string> instructionList = inputLines[i].Split().ToList();
                 iterator = 0;
                 while (instructionList[iterator] == "" && instructionList.Count > 1)
                     iterator++;
@@ -49,7 +52,14 @@ namespace jazTranslator
                         copy();
                         break;
                     case "label":
-                        label(instructionList);
+                        if (inputLines[i + 1].Split().ToList()[0] == "")
+                        {
+                            label_function(instructionList);
+                        }
+                        else
+                        {
+                            label(instructionList);
+                        }
                         break;
                     case "goto":
                         go_to(instructionList);
@@ -65,6 +75,18 @@ namespace jazTranslator
                         break;
                     case "+":
                         plus(instructionList);
+                        break;
+                    case "-":
+                        minus(instructionList);
+                        break;
+                    case "print":
+                        print();
+                        break;
+                    case "call":
+                        call(instructionList);
+                        break;
+                    case "return":
+                        return_();
                         break;
                     default:
                         break;
@@ -86,77 +108,269 @@ namespace jazTranslator
                 newInstruction = newInstruction + " " + instruction[i];
             }
             newInstruction = newInstruction + "\" << endl;";
-            outputLines.Add(newInstruction);
+            if (outputListTracker == 0)
+                outputLines.Add(newInstruction);
+            else
+                functionLines.Add(newInstruction);
+        }
+        static void print()
+        {
+            if (outputListTracker == 0)
+                outputLines.Add("cout << int_stack.top() << endl;");
+            else
+                functionLines.Add("cout << int_stack.top() << endl;");
         }
         static void push(List<string> instruction)
         {
             string value = instruction[iterator + 1];
-            outputLines.Add("int_stack.push(" + value + ");");
+            
+
+            if (outputListTracker == 0)
+                outputLines.Add("int_stack.push(" + value + ");");
+            else
+                functionLines.Add("int_stack.push(" + value + ");");
         }
         static void rvalue(List<string> instruction)
         {
             string variable = instruction[iterator + 1];
-            outputLines.Add("int_stack.push(" + variable + ");");
+
+            if (outputListTracker == 0)
+                outputLines.Add("int_stack.push(" + variable + ");");
+            else
+                functionLines.Add("int_stack.push(" + variable + ");");
+
         }
         static void lvalue(List<string> instruction)
         {
             string variable = instruction[iterator + 1];
-            outputLines.Add("pointer_stack.push(&" + variable + ");");
+           
+            if (outputListTracker == 0)
+                outputLines.Add("pointer_stack.push(&" + variable + ");");
+            else
+                functionLines.Add("pointer_stack.push(&" + variable + ");");
         }
         static void equals()
         {
-            outputLines.Add("*(int *)pointer_stack.top() = int_stack.top();");
-            outputLines.Add("pointer_stack.pop();");
-            outputLines.Add("int_stack.pop();");
+            if (outputListTracker == 0)
+            {
+                outputLines.Add("*(int *)pointer_stack.top() = int_stack.top();");
+                outputLines.Add("pointer_stack.pop();");
+                outputLines.Add("int_stack.pop();");
+            }
+            else
+            {
+                functionLines.Add("*(int *)pointer_stack.top() = int_stack.top();");
+                functionLines.Add("pointer_stack.pop();");
+                functionLines.Add("int_stack.pop();");
+            }
         }
         static void pop()
         {
-            outputLines.Add("int_stack.pop();");
+            if (outputListTracker == 0)
+            {
+                outputLines.Add("int_stack.pop();");
+            }
+            else
+            {
+                functionLines.Add("int_stack.pop();");
+            }
         }
         static void copy()
         {
-            outputLines.Add("int copy = int_stack.top();");
-            outputLines.Add("int_stack.push(copy);");
+        
+
+            if (outputListTracker == 0)
+            {
+                outputLines.Add("int copy = int_stack.top();");
+                outputLines.Add("int_stack.push(copy);");
+            }
+            else
+            {
+                functionLines.Add("int copy = int_stack.top();");
+                functionLines.Add("int_stack.push(copy);");
+            }
         }
         static void label(List<string> instruction)
         {
-            outputLines.Add("label_" + instruction[iterator + 1] + ":");
+            if (outputListTracker == 0)
+            {
+                outputLines.Add("label_" + instruction[iterator + 1] + ":");
+            }
+            else
+            {
+                functionLines.Add("label_" + instruction[iterator + 1] + ":");
+            }
+        }
+        static void label_function(List<string> instruction)
+        {
+            functionLines.Add("void " + instruction[iterator + 1] + "() {");
+            outputListTracker = 1;
         }
         static void go_to(List<string> instruction)
+        {           
+            if (outputListTracker == 0)
+            {
+                outputLines.Add("goto label_" + instruction[iterator + 1] + ";");
+            }
+            else
+            {
+                functionLines.Add("goto label_" + instruction[iterator + 1] + ";");
+            }
+
+        }
+        static void call(List<string> instruction)
         {
-            outputLines.Add("goto label_" + instruction[iterator + 1] +";");
+            if (outputListTracker == 0)
+            {
+                outputLines.Add(instruction[iterator + 1] + " ();");
+            }
+            else
+            {
+                functionLines.Add(instruction[iterator + 1] + " ();");
+            }
         }
         static void gofalse(List<string> instruction)
-        {
-            outputLines.Add("if (int_stack.top() == 0) {");
-            outputLines.Add("goto label_" + instruction[iterator + 1] + "; }");
-            outputLines.Add("int_stack.pop();");
+        {        
+            if (outputListTracker == 0)
+            {
+                outputLines.Add("if (int_stack.top() == 0) {");
+                outputLines.Add("goto label_" + instruction[iterator + 1] + "; }");
+                outputLines.Add("int_stack.pop();");
+
+            }
+            else
+            {
+                functionLines.Add("if (int_stack.top() == 0) {");
+                functionLines.Add("goto label_" + instruction[iterator + 1] + "; }");
+                functionLines.Add("int_stack.pop();");
+
+            }
         }
         static void gotrue(List<string> instruction)
         {
-            outputLines.Add("if (int_stack.top() != 0) {");
-            outputLines.Add("goto label_" + instruction[iterator + 1] + "; }");
-            outputLines.Add("int_stack.pop();");
+            if (outputListTracker == 0)
+            {
+                outputLines.Add("if (int_stack.top() != 0) {");
+                outputLines.Add("goto label_" + instruction[iterator + 1] + "; }");
+                outputLines.Add("int_stack.pop();");
+            }
+            else
+            {
+                functionLines.Add("if (int_stack.top() != 0) {");
+                functionLines.Add("goto label_" + instruction[iterator + 1] + "; }");
+                functionLines.Add("int_stack.pop();");
+            }
         }
         static void halt()
         {
-            outputLines.Add("return 0;");
+            if (outputListTracker == 0)
+            {
+                outputLines.Add("return 0;");
+            }
+            else
+            {
+                functionLines.Add("return 0;");
+            }
+        }
+        static void return_()
+        {
+            functionLines.Add("}");
+            outputListTracker = 0;
         }
         static void plus(List<string> instruction)
         {
-            string variable1 = variables[variableCount];
-            variableCount++;
-  
-            outputLines.Add( variable1 + " = int_stack.top();");
-            outputLines.Add("int_stack.pop();");
+            string variable1, variable2;
+            if (variableCount < variables.Count)
+            {
+                variable1 = variables[variableCount];
+                variableCount++;
+            }
+            else
+            {
+                variable1 = "int " + RandomString(10);
+            }
 
-            string variable2 = variables[variableCount];
-            variableCount++;
+            if (outputListTracker == 0)
+            {
+                outputLines.Add(variable1 + " = int_stack.top();");
+                outputLines.Add("int_stack.pop();");
+            }
+            else
+            {
+                functionLines.Add(variable1 + " = int_stack.top();");
+                functionLines.Add("int_stack.pop();");
+            }
 
-            outputLines.Add( variable2 + " = int_stack.top();");
-            outputLines.Add("int_stack.pop();");
-            outputLines.Add("int_stack.push(" + variable1 + " + " + variable2 + ");");
+            if (variableCount < variables.Count)
+            {
+                variable2 = variables[variableCount];
+                variableCount++;
+            }
+            else
+            {
+                variable2 = "int " + RandomString(10);
+            }
 
+            if (outputListTracker == 0)
+            {
+                outputLines.Add(variable2 + " = int_stack.top();");
+                outputLines.Add("int_stack.pop();");
+                outputLines.Add("int_stack.push(" + variable1 + " + " + variable2 + ");");
+            }
+            else
+            {
+                functionLines.Add(variable2 + " = int_stack.top();");
+                functionLines.Add("int_stack.pop();");
+                functionLines.Add("int_stack.push(" + variable1 + " + " + variable2 + ");");
+            }
+        }
+
+        static void minus(List<string> instruction)
+        {
+            string variable1, variable2;
+            if (variableCount < variables.Count)
+            {
+                variable1 = variables[variableCount];
+                variableCount++;
+            }
+            else
+            {
+                variable1 = RandomString(10);
+            }
+
+            if (outputListTracker == 0)
+            {
+                outputLines.Add(variable1 + " = int_stack.top();");
+                outputLines.Add("int_stack.pop();");
+            }
+            else
+            {
+                functionLines.Add(variable1 + " = int_stack.top();");
+                functionLines.Add("int_stack.pop();");
+            }
+
+            if (variableCount < variables.Count)
+            {
+                variable2 = variables[variableCount];
+                variableCount++;
+            }
+            else
+            {
+                variable2 = "int " + RandomString(10);
+            }
+
+            if (outputListTracker == 0)
+            {
+                outputLines.Add(variable2 + " = int_stack.top();");
+                outputLines.Add("int_stack.pop();");
+                outputLines.Add("int_stack.push(" + variable1 + " - " + variable2 + ");");
+            }
+            else
+            {
+                functionLines.Add(variable2 + " = int_stack.top();");
+                functionLines.Add("int_stack.pop();");
+                functionLines.Add("int_stack.push(" + variable1 + " - " + variable2 + ");");
+            }
         }
         static void initialzeProgram()
         {
@@ -165,38 +379,44 @@ namespace jazTranslator
                 File.Delete("output.cpp");
             }
 
-            string line;
             string instruction;
-            StreamReader file = new StreamReader(@"jaz specs and examples (1)\demo.jaz");
-            while ((line = file.ReadLine()) != null)
+            for (int i = 0; i < inputLines.Count(); i++) //this is a loop to check through the lines before compiling
             {
-                List<string> instructionList = line.Split().ToList();
+                List<string> instructionList = inputLines[i].Split().ToList();
                 iterator = 0;
                 while (instructionList[iterator] == "" && instructionList.Count > 1)
                     iterator++;
                 instruction = instructionList[iterator];
 
-                if (instruction == "lvalue" && !variables.Contains(instructionList[iterator + 1]))
+                if ((instruction == "lvalue" || instruction == "rvalue") && !variables.Contains(instructionList[iterator + 1]))
                 {
                     variables.Add(instructionList[iterator + 1]);
                 }
+                if (instruction == "label" && inputLines[i + 1].Split().ToList()[0] == "")
+                {
+                    functions.Add(instructionList[iterator + 1]);
+                }
             }
 
-                using (StreamWriter sw = File.CreateText("output.cpp"))
+            using (StreamWriter sw = File.CreateText("output.cpp"))
             {
                 sw.WriteLine("# include \"stdafx.h\"");
                 sw.WriteLine("# include <iostream>");
                 sw.WriteLine("# include <string>");
                 sw.WriteLine("# include <stack>");
                 sw.WriteLine("using namespace std;");
-                sw.WriteLine("int main() {");
 
-                for (int i  = 0; i < variables.Count; i++)
+                for (int i = 0; i < variables.Count; i++)
                 {
                     sw.WriteLine("int " + variables[i] + ";");
                 }
+                for (int i = 0; i < functions.Count; i++)
+                {
+                    sw.WriteLine("void " + functions[i] + "();");
+                }
                 sw.WriteLine("stack<int> int_stack;");
                 sw.WriteLine("stack<int*> pointer_stack;");
+                sw.WriteLine("int main() {");
             }
         }
         static void finishProgram()
@@ -207,16 +427,31 @@ namespace jazTranslator
                 {
                     sw.WriteLine(outputLines[i]);
                 }
+
                 sw.WriteLine("}");
+
+                for (int i = 0; i < functionLines.Count; i++)
+                {
+                    sw.WriteLine(functionLines[i]);
+                }
                 sw.Close();
             }
         }
-       
+        static void readJazLines()
+        {
+            string line;
+            StreamReader file = new StreamReader(@"jaz specs and examples (1)\operatorsTest.jaz");
+            while ((line = file.ReadLine()) != null)
+            {
+                inputLines.Add(line);
+            }
+        }
         public static string RandomString(int length)
         {
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
             return new string(Enumerable.Repeat(chars, length)
-              .Select(s => s[random.Next(s.Length)]).ToArray());
+               .Select(s => s[random.Next(s.Length)]).ToArray());
         }
+
     }
 }
